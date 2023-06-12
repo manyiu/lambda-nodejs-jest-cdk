a40db3160696dae02609fe9c73270a3ba51c21cd
Man Yiu <manyiu.wong@imbee.io>
2023-06-12 15:10:28 +0800

    add lambda and layer in cdk stack

--- lambdas/hello/index.ts
+++ lambdas/hello/index.ts
@@ -3,7 +3,12 @@ import { add } from "@layer/index";
 
 export const handler = async (event: any) => {
   const response = await axios.get("https://www.google.com");
+
   console.log(add(1, 2));
 
-  return response.status;
+  return {
+    status: response.status,
+    data: response.data,
+    add: add(1, 2),
+  };
 };
--- lib/lambda-nodejs-jest-cdk-stack.ts
+++ lib/lambda-nodejs-jest-cdk-stack.ts
@@ -1,16 +1,34 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdaNodejsJestCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaLayer = new cdk.aws_lambda.LayerVersion(
+      this,
+      "LambdaNodejsJestCdkLayer",
+      {
+        compatibleRuntimes: [cdk.aws_lambda.Runtime.NODEJS_18_X],
+        compatibleArchitectures: [cdk.aws_lambda.Architecture.ARM_64],
+        code: cdk.aws_lambda.Code.fromAsset("lambdas/layers/base"),
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaNodejsJestCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const helloLambda = new cdk.aws_lambda_nodejs.NodejsFunction(
+      this,
+      "HelloLambda",
+      {
+        entry: "lambdas/hello/index.ts",
+        handler: "handler",
+        runtime: cdk.aws_lambda.Runtime.NODEJS_18_X,
+        bundling: {
+          tsconfig: "lambdas/tsconfig.json",
+          externalModules: ["@aws-sdk/*", "axios"],
+        },
+        layers: [lambdaLayer],
+      }
+    );
   }
 }
